bb2c77d83193bec583174d7ecc90b0b2f9fb29c5
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-11-12 23:42:31 +0530

    initalize invoices app and models along with migrations

---
+++ invoices/models.py
@@ -0,0 +1,21 @@
+import uuid
+
+from django.db import models
+
+
+# Create your models here.
+
+class Invoice(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    invoice_number = models.CharField(max_length=255, unique=True)
+    customer_number = models.CharField(max_length=255)
+    invoice_date = models.DateField()
+
+
+class InvoiceDetails(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=20,decimal_places=2)
+    line_total = models.DecimalField(max_digits=20, decimal_places=2)
